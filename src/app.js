6e929aa075fb15aa003dd5fcee2e057d13389a51
Bo Zhao <zhaobo@uw.edu>
2023-06-17 13:53:20 -0700

    add the support for github.io

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const app = express();
 const index = require('./routes/index');
 const userRoute = require('./routes/product.routes');
+let corsOptions = {
+    origin: ['http://localhost:3000', 'https://www.github.io', 'http://www.github.io'],
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,7 +15,7 @@ app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(index);
 app.use('/api/',userRoute);
 
-app.use('/', express.static('docs'))
+app.use('/', express.static('client'))
 
 module.exports = app;
 
