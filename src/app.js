94fa4c83cb5e52f9e6489d7171e0692cbceca8b8
Bo Zhao <zhaobo@uw.edu>
2023-06-17 13:21:15 -0700

    update

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ const cors = require('cors');
 const app = express();
 const index = require('./routes/index');
 const userRoute = require('./routes/product.routes');
-let corsOptions = {
-    origin: ['https://jakobzhao.github.io', 'http://127.0.0.1:5501',
-            'http://127.0.0.1:53369', 'http://127.0.0.1:3000', 'http://localhost:3000','https://localhost:3000', 'http://127.0.0.1:5500', 'http://hgis.uw.edu','https://hgis.uw.edu' ],
-    optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions));
+// let corsOptions = {
+//     origin: ['http://localhost:3000'],
+//     optionsSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
